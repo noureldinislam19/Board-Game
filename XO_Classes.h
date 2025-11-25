f6e65126e99265cca4b305da49fd80480395a555
NourEldin Islam <noureldinislam19@gmail.com>
2025-11-25 06:09:29 +0200

    Add files via upload

--- XO_Classes.h
+++ XO_Classes.h
@@ -52,7 +52,7 @@ public:
 
 class X_O_Num_Board : public Board<char> {
 private:
-    char blank_symbol = '.';
+    char blank_symbol = '0';
     map<int, bool> used_numbers;
 	
 
@@ -70,5 +70,28 @@ class XO_Num_UI : public UI<char> {
     XO_Num_UI();
     ~XO_Num_UI() {};
     Player<char>* create_player(string& name, char symbol, PlayerType type);
+    virtual Move<char>* get_move(Player<char>* player);
+};
+
+class SUS_Board : public Board<char> {
+private:
+    char blank_symbol = '.';
+    int sus_count[2] = { 0,0 };// count SUS made by each player to detect winner
+public:
+    SUS_Board();
+    bool update_board(Move<char>* move);
+    bool is_win(Player<char>* player);
+    bool is_lose(Player<char>*);
+    bool is_draw(Player<char>* player);
+    bool game_is_over(Player<char>* player);
+    int check(pair<int, int>, pair<int, int>, pair<int, int>);
+};
+
+class SUS_UI : public UI<char> {
+public:
+    SUS_UI();
+    ~SUS_UI() {};
+    Player<char>* create_player(string& name, char symbol, PlayerType type);
+    Player<char>** setup_players()override;
     virtual Move<char>* get_move(Player<char>* player);
 };
--- XO_Demo.cpp
+++ XO_Demo.cpp
@@ -137,7 +137,40 @@ int main() {
         // Delete the dynamically allocated array of player pointers itself.
         delete[] players;
         return 0; // Exit successfully
-	}
+    }
+    else if (x == 1) {
+        srand(static_cast<unsigned int>(time(0)));  // Seed the random number generator
+
+        // Create an instance of the specific UI for X-O using a pointer 
+        UI<char>* game_ui = new SUS_UI();
+
+        // Create the game board. For X-O, this is an X_O_Board.
+        Board<char>* sus_board = new SUS_Board();
+
+        // Use the UI to set up the players for the game.
+        // The UI returns a dynamically allocated array of Player pointers.
+        Player<char>** players = game_ui->setup_players();
+
+
+        // Create the game manager with the board and the array of players.
+        GameManager<char> sus_game(sus_board, players, game_ui);
+
+        // Run the game loop.
+        sus_game.run();
+
+        // --- Cleanup ---
+        // Delete the dynamically allocated board object.
+        delete sus_board;
+
+        // Delete the individual player objects.
+        for (int i = 0; i < 2; ++i) {
+            delete players[i];
+        }
+        // Delete the dynamically allocated array of player pointers itself.
+        delete[] players;
+
+        return 0;
+    }
 }
 
 // ========================================
